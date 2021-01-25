6c8906a271a7fedf3c1a5894112c4bfa97b7c67d
Shunze Lin <4A490101@stust.edu.tw>
2021-01-25 17:56:51 +0800

    Fixing CORS bug

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', 'https://app0529-43866.web.app');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Credentials', 'true');
 	next();
 });
 app.use(compression());
