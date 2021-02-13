58d053fc6b4e83aa3d64e7f38a4b9ad057126095
Shunze Lin <4A490101@stust.edu.tw>
2021-02-13 22:40:44 +0800

    test: Prepare development code to heroku

--- server/app.js
+++ server/app.js
@@ -10,26 +10,26 @@ import xss from 'xss-clean';
 
 const app = express();
 app.use(helmet());
-// app.enable('trust proxy');
-// app.use(
-// 	cors({
-// 		origin: 'https://app0746-9a789.web.app',
-// 		credentials: true
-// 	})
-// );
-// app.options('*', cors());
-// app.use(function (req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', 'https://app0746-9a789.web.app');
-// 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-// 	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-// 	res.header('Access-Control-Allow-Credentials', true);
-// 	next();
-// });
+app.enable('trust proxy');
+app.use(
+	cors({
+		origin: 'https://app0746-9a789.web.app',
+		credentials: true
+	})
+);
+app.options('*', cors());
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://app0746-9a789.web.app');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	res.header('Access-Control-Allow-Credentials', true);
+	next();
+});
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
-// app.use(compression());
+app.use(compression());
 app.use(mongoSanitize());
 app.use(xss());
 const __dirname = path.resolve();
