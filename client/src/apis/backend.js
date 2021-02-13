47bd56a8b7a116537e9add474082e2cb9507406b
Shunze Lin <4A490101@stust.edu.tw>
2021-02-13 22:42:51 +0800

    test: Prepare Front-end development code to firebase

--- client/src/apis/backend.js
+++ client/src/apis/backend.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const authRequest = axios.create({
-	baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/users`
-	//withCredentials: true
+	baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/users`,
+	{withCredentials: true}
 });
 
 const productRequest = axios.create({
@@ -11,17 +11,17 @@ const productRequest = axios.create({
 
 const reviewRequest = (productId) =>
 	axios.create({
-		baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/products/${productId}/reviews`
-		// {
-		// 	withCredentials: true
-		// }
+		baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/products/${productId}/reviews`,
+		{
+			withCredentials: true
+		}
 	});
 
 const orderRequest = axios.create({
-	baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/orders`
-	//{
-	//    withCredentials: true
-	//}
+	baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/v1/orders`,
+	{
+	   withCredentials: true
+	}
 });
 
 export { authRequest, productRequest, reviewRequest, orderRequest };
