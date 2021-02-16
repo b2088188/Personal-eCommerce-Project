24df38b64925199b83f6c624fc8d0004d3e47650
Shunze Lin <4A490101@stust.edu.tw>
2021-02-16 16:06:00 +0800

    bugfix:Fixing the bug of creating time for models

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -2,96 +2,98 @@ import mongoose from 'mongoose';
 import validator from 'validator';
 
 const orderSchema = new mongoose.Schema({
-    user: {
-    	type: mongoose.Schema.ObjectId,
-    	ref: 'User',
-    	required: [true, 'An order must belong to a user']
-    },
-    orderItems: [{
-        product: {
+   user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'An order must belong to a user']
+   },
+   orderItems: [
+      {
+         product: {
             type: mongoose.Schema.ObjectId,
             required: [true, 'An item must have a product ID'],
             ref: 'Product'
-        },
-        name: {
+         },
+         name: {
             type: String,
             required: [true, 'An item must have a name']
-        },
-        image: {
+         },
+         image: {
             type: String,
             required: [true, 'An item must have an image']
-        },
-        price: {
+         },
+         price: {
             type: Number,
             required: [true, 'An item must have a price']
-        },
-        quantity: {
+         },
+         quantity: {
             type: Number,
             required: [true, 'An item must have a at least one quantity']
-        }
-    }],
-    itemsPrice: {
-    	type: Number,
-    	required: [true, 'An order must have an itemsPrice']
-    },
-    shippingPrice: {
-    	type: Number,
-    	required: [true, 'An order must have a shippingPrice']
-    },
-    totalPrice: {
-    	type: Number,
-    	required: [true, 'An order must have a totalPrice']
-    },    
-    shippingAddress: {
-    	address: {
-    		type: String,
-    		required: [true, 'Please provide an address for shipping']
-    	},
-    	city: {
-    		type: String,
-    		required: [true, 'Please provide a city name for shipping']
-    	},
-    	postalCode: {
-    		type: String,
-    		required: [true, 'Please provide a postalCode for shipping']
-    	},
-    	country: {
-    		type: String,
-    		required: [true, 'Please provide a country for shipping']
-    	}
-    },
-    paymentMethod: {
-    	type: String,
-    	required: [true, 'Please provide a paymentMethod']
-    },
-    paymentResult: {
-    	id: String,
-    	status: String
-    },
-    isPaid: {
-    	type: Boolean,
-    	default: false
-    },
-    paidAt: {
-    	type: Date
-    },
-    isDelivered: {
-    	type: Boolean,
-    	default: false
-    },
-    deliveredAt: {
-    	type: Date
-    },
-    createdAt: {
-    	type: Date,
-    	default: Date.now()
-    }    
-})
+         }
+      }
+   ],
+   itemsPrice: {
+      type: Number,
+      required: [true, 'An order must have an itemsPrice']
+   },
+   shippingPrice: {
+      type: Number,
+      required: [true, 'An order must have a shippingPrice']
+   },
+   totalPrice: {
+      type: Number,
+      required: [true, 'An order must have a totalPrice']
+   },
+   shippingAddress: {
+      address: {
+         type: String,
+         required: [true, 'Please provide an address for shipping']
+      },
+      city: {
+         type: String,
+         required: [true, 'Please provide a city name for shipping']
+      },
+      postalCode: {
+         type: String,
+         required: [true, 'Please provide a postalCode for shipping']
+      },
+      country: {
+         type: String,
+         required: [true, 'Please provide a country for shipping']
+      }
+   },
+   paymentMethod: {
+      type: String,
+      required: [true, 'Please provide a paymentMethod']
+   },
+   paymentResult: {
+      id: String,
+      status: String
+   },
+   isPaid: {
+      type: Boolean,
+      default: false
+   },
+   paidAt: {
+      type: Date
+   },
+   isDelivered: {
+      type: Boolean,
+      default: false
+   },
+   deliveredAt: {
+      type: Date
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now
+   }
+});
 
 orderSchema.methods.processOrder = function (body) {
-    this.isPaid = true;
-    this.paidAt = Date.now();
-    this.paymentResult = body.paymentResult;
-}
+   this.isPaid = true;
+   this.paidAt = Date.now();
+   this.paymentResult = body.paymentResult;
+};
 const Order = mongoose.model('Order', orderSchema);
 export default Order;
